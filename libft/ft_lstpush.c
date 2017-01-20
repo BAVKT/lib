c5770e1c20e52abe8b1b984320acfc5c03519c81
Vincent MERCADIER <vmercadi@e3r11p11.42.fr>
2017-01-20 14:02:12 +0100

    Modification de lstpush

--- libft/ft_lstpush.c
+++ libft/ft_lstpush.c
@@ -6,15 +6,15 @@
 /*   By: vmercadi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/12/14 11:50:37 by vmercadi          #+#    #+#             */
-/*   Updated: 2016/12/22 12:46:13 by vmercadi         ###   ########.fr       */
+/*   Updated: 2017/01/20 14:01:48 by vmercadi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_lstpush(t_list lst, t_list new)
+void	ft_lstpush(t_list *lst, t_list new)
 {
 	while (lst->next)
-		lst = lst->new;
+		lst = lst->next;
 	lst->next = new;
 }
